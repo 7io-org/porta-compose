f0d558a7670386091af26b1752dce2a4f1df3464
Kaede Fujisaki <kaede@hexe.net>
2020-09-17 19:20:35 +0900

    update resolver

---
+++ image/entrypoint.sh
@@ -0,0 +1,5 @@
+export NAMESERVER=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
+
+envsubst '$NAMESERVER' < /resolver.conf.template > /etc/nginx/conf.d/resolver.conf
+
+nginx -g "daemon off;"
