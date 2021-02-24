72b40582eecd9bee0c248858499d7c178e009c68
Kaede Fujisaki <kaede@hexe.net>
2021-02-24 23:33:04 +0900

    renamed

--- image/nginx/entrypoint.sh
+++ image/nginx/entrypoint.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 export NAMESERVER=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
 
 envsubst '$NAMESERVER' < /resolver.conf.template > /etc/nginx/conf.d/resolver.conf
