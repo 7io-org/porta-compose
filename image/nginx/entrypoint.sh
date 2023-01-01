e1f53ea7e9253d904be025cb4adc5b410f89fbfb
Kaede Fujisaki <kaede@hexe.net>
2023-01-01 22:30:20 +0900

    cleanup image

--- image/nginx/entrypoint.sh
+++ image/nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export NAMESERVER=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
+export NAMESERVER="$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ' | sed 's/^ *\| *$//')"
 
 envsubst '$NAMESERVER' < /resolver.conf.template > /etc/nginx/conf.d/resolver.conf
 
